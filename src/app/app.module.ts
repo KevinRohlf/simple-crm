cc87e3b418a41d3285a07efa453141595b9701ba
Kevin Rohlf <123676073+KevinRohlf@users.noreply.github.com>
2023-06-02 00:46:33 +0200

    added routing | add user button |  adduser form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
+import { DialogAddUserComponent } from './dialog-add-user/dialog-add-user.component';
+import { MatNativeDateModule } from '@angular/material/core';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    UserComponent,
+    DialogAddUserComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +33,15 @@ import {MatIconModule} from '@angular/material/icon';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { User } from 'src/models/user.class';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  templateUrl: './dialog-add-user.component.html',
+  styleUrls: ['./dialog-add-user.component.scss']
+})
+export class DialogAddUserComponent {
+  user = new User();
+
+  save() {
+    console.log('current user is: ', this.user)
+  }
+
+}
